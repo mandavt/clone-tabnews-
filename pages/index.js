d1e3bb2a31c76638a86385ae11ff100a08fed01c
Amanda Matos <amandavictorinomor@gmail.com>
2025-07-29 16:26:07 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
-﻿function Home(){
-    return <h1>Bora?</h1>
+﻿import React from 'react'
+
+function Home() {
+    const [name, setName] = React.useState('')
+
+    return <>
+        <h1>Let's bora?</h1>
+        <Input />
+        <Button>Aoba</Button>
+
+        {/* <input type="text" value={name} onChange={event => setName(event.target.value)} /> */}
+    </>
+
+}
+
+export function Button(props) {
+    return <button>{props.children}</button>
+}
+
+export function Input() {
+    return <input type="text" />
 }
 
 export default Home;
